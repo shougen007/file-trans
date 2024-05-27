46ebf9e4b9ebc7a50d3b69ba8797f1f18d2b5f1e
sanghyun <son@sanghyuns-Mac-mini.local>
2024-05-27 10:08:07 +0900

    Settings

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://poetic-salmon-66.clerk.accounts.dev",
+      domain: "https://smart-dragon-5.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "adventurous-caiman-790.convex.cloud",
+        hostname: "loyal-salamander-140.convex.cloud",
       },
     ],
   },
